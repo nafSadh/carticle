6385dcfc076e2289d0784af12be9668df77b2e36
nafSadh <khan@nafsadh.com>
2014-06-23 23:25:58 -0400

    testing sdk

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-echo "hello";
-$my_env_var = getenv('MY_VAR');
-echo $my_env_var;
+echo "hello<br/>";
+
+include_once 'EdmundsAPI-sdk-js2php-port/edmunds.api.sdk.php';
+
+$edpi = new EdmundsAPI('4gcfhrfgmz35qfxsrp9rg9df');
+echo $edpi->call('api/vehicle/v2/makes','');
 ?>
---
+++ php.ini
@@ -0,0 +1,3 @@
+<?php
+$define("edmundsAPIk","4gcfhrfgmz35qfxsrp9rg9df");
+?>
